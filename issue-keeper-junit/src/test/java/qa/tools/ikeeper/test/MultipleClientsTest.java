46547cfc67b3f2672987aabd74183c912fef6eb0
Tomáš Livora <livoratom@gmail.com>
2016-09-14 12:53:33 +0200

    Fix problem with multiple clients (#10)
    Only the first client was used and the other ones were always ignored.

---
+++ issue-keeper-junit/src/test/java/qa/tools/ikeeper/test/MultipleClientsTest.java
@@ -0,0 +1,56 @@
+package qa.tools.ikeeper.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.Test;
+import qa.tools.ikeeper.annotation.BZ;
+import qa.tools.ikeeper.annotation.Jira;
+import qa.tools.ikeeper.client.BugzillaClient;
+import qa.tools.ikeeper.client.JiraClient;
+
+public class MultipleClientsTest {
+
+    private static final List<String> executed = new ArrayList<String>();
+
+    @Rule
+    public IKeeperJUnitConnector issueKeeper = new IKeeperJUnitConnector(
+            new BugzillaClient("https://bugzilla.redhat.com"),
+            new JiraClient("https://issues.jboss.org")
+    );
+
+    @AfterClass
+    public static void checkExecutions() {
+        Assertions.assertThat(executed).containsOnly("runVerifiedBugzillaTest", "runVerifiedJiraTest");
+    }
+
+    @BZ("1155593")
+    @Test
+    public void runVerifiedBugzillaTest() {
+        executed.add("runVerifiedBugzillaTest");
+    }
+
+    @BZ("1217371")
+    @Test
+    public void ignoreNewBugzillaTest() {
+        executed.add("ignoreNewBugzillaTest");
+        System.out.println("ignoreNewBugzillaTest");
+    }
+
+    @Jira("JBPM-4608")
+    @Test
+    public void runVerifiedJiraTest() {
+        executed.add("runVerifiedJiraTest");
+    }
+
+    @Jira("JBPM-4198")
+    @Test
+    public void ignoreNewJiraTest() {
+        executed.add("ignoreNewJiraTest");
+        System.out.println("ignoreNewJiraTest");
+    }
+
+}
